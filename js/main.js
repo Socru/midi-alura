c895a3ab47ed197a838f4c2767a81bb9eeb4c9da
Ricardo Socru <socructr@gmail.com>
2024-07-09 20:51:57 -0600

    Add files via upload
    actualizacion funciones

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-function playPom(){
+function playSound(idElementAudio){
 
-document.querySelector('#sonido_tecla_pom').play(); }
-document.querySelector('.tecla_pom').onclick=playPom;
+    document.querySelector(idElementAudio).play();
+}
+
+const listateclas = document.querySelectorAll('.tecla');
+
+let x = 0
+while (x < 9) {
+    const teclas = listateclas[x]
+    const instumento = teclas.classList[1];
+
+    const idAudio=`#sonido_${instumento}`;
+
+    teclas.onclick = function () {
+        playSound(idAudio);
+     };
+    x++;
+}
+
