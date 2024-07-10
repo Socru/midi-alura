2e3fe9eef0610a2040072cabfb823f0156412de3
Ricardo Socru <socructr@gmail.com>
2024-07-09 22:11:20 -0600

    Add files via upload
    finalizado

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function playSound(idElementAudio){
 }
 
 const listateclas = document.querySelectorAll('.tecla');
-
+/*
 let x = 0
 while (x < 9) {
     const teclas = listateclas[x]
@@ -17,4 +17,24 @@ while (x < 9) {
      };
     x++;
 }
+*/
+for (let i = 0; i < 9; i++){
+    const teclas = listateclas[i]
+    const instumento = teclas.classList[1];
 
+    const idAudio=`#sonido_${instumento}`;
+
+    teclas.onclick = function () {
+        playSound(idAudio);
+    };
+    
+    teclas.onkeydown = function (evento) {
+        if(evento.code === 'Space'|| evento.code === 'Enter'){
+        teclas.classList.add('activa');
+        }
+        
+    }
+    teclas.onkeyup = function(){
+        teclas.classList.remove('activa');
+}
+}
